6ccf22e010b7ca78182cd09834f91d3bd5e7d50e
dylan-calle <81180803+dylan-calle@users.noreply.github.com>
2024-09-21 10:10:11 -0400

    Delete Pharmacy-backend/hola directory

--- Pharmacy-backend/hola/database/database.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from "mysql2/promise";
-
-export const getConnection = createPool({
-  host: "localhost",
-  user: "dylan",
-  password: "080376Crp!",
-  port: 3306,
-  database: "pharmacy",
-});
--- Pharmacy-backend/hola/routes/addMerma.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { metodos as addMermaController } from "../controllers/addMerma.controller.js";
-
-const router = Router();
-router.get("/getNumber", addMermaController.getNumber);
-router.get("/getMaterial", addMermaController.getMaterial);
-router.post("/mermaAndQuantity", addMermaController.mermaAndQuantity);
-
-export default router;
